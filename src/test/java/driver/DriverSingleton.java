0e75ae0c3ce9ec214cfb9b0ca1c3711389846849
d.grischenko <ext.grischenko@tinkoff.ru>
2022-06-06 09:11:12 +0300

    add some updates

--- src/test/java/driver/DriverSingleton.java
+++ src/test/java/driver/DriverSingleton.java
@@ -3,8 +3,6 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
